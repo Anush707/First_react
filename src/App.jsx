d8d7c9b13bffc078414398521161ad7d00546de7
Anush Gotame <anushgotame333@gmail.com>
2024-12-21 19:40:13 +0200

    Second Day

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>Helo</div>
-  )
+import KgButton from './KgButton';
+function App() {
+  return <div>
+    <h1>React course</h1>
+    <KgButton></KgButton>
+  </div>
 }
-
-export default App
+export default App;
